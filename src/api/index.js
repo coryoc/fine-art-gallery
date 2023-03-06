11ad17a4b5de0d815f3c78381ed058c51d4a414b
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-03-06 10:38:12 -0600

    MVP deployed

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,8 @@
-/**
- * This file features and exports all of your calls to the API
- * 
- * You need to replace YOUR_API_KEY in the string associated with KEY with your actual API key
- */
-export const BASE_URL = 'https://api.harvardartmuseums.org';
-export const KEY = 'apikey=YOUR_API_KEY';
 
-/**
- * This will make a call to the API for a single term and value (e.g. "person", and "unknown"), and return the result
- */
+export const BASE_URL = 'https://api.harvardartmuseums.org';
+export const KEY = 'apikey=71377cad-a278-4cc2-b136-d0e416739ed5';
+
+
 export async function fetchQueryResultsFromTermAndValue(term, value) {
   try {
     const response = await fetch(`${ BASE_URL }/object?${ KEY }&${ term }=${ encodeURI(value.split('-').join('|')) }`);
@@ -20,9 +14,7 @@ export async function fetchQueryResultsFromTermAndValue(term, value) {
   }
 }
 
-/**
- * This will make a call to the API for a preformed url (useful for previous and next buttons), and return the result
- */
+
 export async function fetchQueryResultsFromURL(url) {
   try {
     const response = await fetch(url);
@@ -34,11 +26,7 @@ export async function fetchQueryResultsFromURL(url) {
   }
 }
 
-/**
- * Requires an object { century: '', classification: '', queryString: '' } to be passed in as an argument
- * 
- * Then makes a call to the API, and returns the first page of results
- */
+
 export async function fetchQueryResults({
   century,
   classification,
@@ -57,9 +45,6 @@ export async function fetchQueryResults({
   }
 }
 
-/**
- * This returns early if there are centuries stored in localStorage, or fetches them from the API and stores them in localStorage if not
- */
 export async function fetchAllCenturies() {
   if (localStorage.getItem('centuries')) {
     return JSON.parse(localStorage.getItem('centuries'));
@@ -80,9 +65,7 @@ export async function fetchAllCenturies() {
   }
 }
 
-/**
- * This returns early if there are classifications stored in localStorage, or fetches them from the API and stores them in localStorage if not 
- */
+
 export async function fetchAllClassifications() {
   if (localStorage.getItem('classifications')) {
     return JSON.parse(localStorage.getItem('classifications'));
--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,36 +1,39 @@
 import React, { Fragment } from 'react';
 
-// Don't touch this import
+
 import { fetchQueryResultsFromTermAndValue } from '../api';
 
-/**
- * We need a new component called Searchable which:
- * 
- * Has a template like this:
- * 
- * <span className="content">
- *  <a href="#" onClick={async (event) => {}}>SOME SEARCH TERM</a>
- * </span>
- *
- * You'll need to read searchTerm, searchValue, setIsLoading, and setSearchResults off of the props.
- * 
- * When someone clicks the anchor tag, you should:
- * 
- * - preventDefault on the event
- * - call setIsLoading, set it to true
- * 
- * Then start a try/catch/finally block:
- * 
- * try:
- *  - await the result of fetchQueryResultsFromTermAndValue, passing in searchTerm and searchValue
- *  - send the result to setSearchResults (which will update the Preview component)
- * catch: 
- *  - console.error the error
- * finally:
- *  - call setIsLoading, set it to false
- */
+
 const Searchable = (props) => {
-  
+    const {
+        searchTerm, 
+        searchValue, 
+        setIsLoading,
+        setSearchResults,
+    } = props;
+
+    return (
+        <span className="content">
+        <a href="#" onClick={async (event) => {
+            event.preventDefault;
+            setIsLoading(true);
+
+                try {
+                    const results = await fetchQueryResultsFromTermAndValue({ searchTerm, searchValue });
+                    setSearchResults(results);
+                } 
+                catch (err) {
+                    console.error(err);
+                }
+                finally {
+                    setIsLoading(false);
+                }
+            }}>
+            { searchTerm }
+        </a>
+        </span>
+
+    )
 }
 
 /**
@@ -67,8 +70,143 @@ const Searchable = (props) => {
  * 
  * This component should be exported as default.
  */
-const Feature = (props) => {
-
-}
+ const Feature = (props) => {
+    const { featuredResult } = props;
+  
+    if (!featuredResult) {
+      return <main id="feature"></main>
+    }
+  
+    const {
+      title, 
+      dated,
+      images,
+      primaryimageurl,
+      description,
+      culture,
+      style,
+      technique,
+      medium,
+      dimensions,
+      people,
+      department,
+      division,
+      contact,
+      creditline,
+    } = featuredResult;
+  
+    return <main id="feature">
+      <div className="object-feature">
+        <header>
+          <h3>{ title }</h3>
+          <h4>{ dated }</h4>
+        </header>
+        <section className="facts">
+          {
+            description
+            ? <Fragment>
+                <span className="title">Description</span>
+                <span className="content">{ description }</span>
+              </Fragment>
+            : null
+          }
+          {
+            culture
+            ? <Fragment>
+                <span className="title">Culture</span>
+                <Searchable searchTerm="culture" searchValue={ culture } { ...props } />
+              </Fragment>
+            : null
+          }
+          {
+            style
+            ? <Fragment>
+                <span className="title">Style</span>
+                <span className="content">{ style }</span>
+              </Fragment>
+            : null
+          }
+          {
+            technique
+            ? <Fragment>
+                <span className="title">Technique</span>
+                <Searchable searchTerm="technique" searchValue={ technique } { ...props } />
+              </Fragment>
+            : null
+          }
+          {
+            medium
+            ? <Fragment>
+                <span className="title">Medium</span>
+                <Searchable searchTerm="medium" searchValue={ medium.toLowerCase() } { ...props } />
+              </Fragment>
+            : null
+          }
+          {
+            dimensions
+            ? <Fragment>
+                <span className="title">Dimensions</span>
+                <span className="content">{ dimensions }</span>
+              </Fragment>
+            : null
+          }
+          {
+            people
+            ? people.map(person => 
+              <Fragment key={ person.displayname }>
+                <span className="title">Person</span>
+                <Searchable searchTerm="person" searchValue={ person.displayname } { ...props } />
+              </Fragment>)
+            : null
+          }
+          {
+            department
+            ? <Fragment>
+                <span className="title">Department</span>
+                <span className="content">{ department }</span>
+              </Fragment>
+            : null
+          }
+          {
+            division
+            ? <Fragment>
+                <span className="title">Division</span>
+                <span className="content">{ division }</span>
+              </Fragment>
+            : null
+          }
+          {
+            contact
+            ? <Fragment>
+                <span className="title">Dimensions</span>
+                <span className="content">
+                  <a target="_blank" rel="noopener noreferrer" href={`mailto:${ contact }`}>{ contact }</a>
+                </span>
+              </Fragment>
+            : null
+          }
+          {
+            creditline
+            ? <Fragment>
+                <span className="title">Credit</span>
+                <span className="content">{ creditline }</span>
+              </Fragment>
+            : null
+          }
+        </section>
+        <section className="photos">
+          {
+            images && images.length > 0 
+            ? images.map((image) => 
+                <img key={ image.baseimageurl } src={ image.baseimageurl } alt={ image.baseimageurl } />
+              )
+            : primaryimageurl
+            ? <img src={ primaryimageurl } alt={ primaryimageurl } />
+            : null
+          }
+        </section>
+      </div>
+    </main>;
+  }
 
 export default Feature;
